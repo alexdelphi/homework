a9092fdcd7109bde0af0056bf3f5b1d6f836fd63
Alexander Edigarev <alexander.edigarew@yandex.ru>
2019-09-08 20:07:40 +0300

    proper structure

---
+++ bin/chrome/youtube.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         yandex fixes
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.youtube.com/*
+// @require      https://raw.githubusercontent.com/alexdelphi/homework/master/launcher/chrome.js
+// @require      https://raw.githubusercontent.com/alexdelphi/homework/master/common/youtube.js
+// @grant        none
+// ==/UserScript==
---
+++ bin/yandex/yandex.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         yandex fixes
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.yandex.ru/
+// @match        https://yandex.ru/
+// @match        yandex.ru/
+// @require      https://raw.githubusercontent.com/alexdelphi/homework/master/launcher/yandex.js
+// @require      https://raw.githubusercontent.com/alexdelphi/homework/master/common/yandex.js
+// @grant        none
+// ==/UserScript==
